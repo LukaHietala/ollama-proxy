1e28fac00eb42ef6716bc062a8ceb9f0d63141de
lukahietala <luka.hietala08@gmail.com>
2025-04-10 10:47:22 +0300

    add send msg and get models methods

--- openwebui/openwebui.py
+++ openwebui/openwebui.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import requests
 
 class OpenWebUIClient:
     def __init__(self, base_url=None, api_key=None, model=None):
@@ -18,9 +19,52 @@ class OpenWebUIClient:
         
         self.headers = {"Content-Type": "application/json"}
         self.headers["Authorization"] = f"Bearer {self.api_key}"
-
+        
+        self.conversation_history = []
+    
+    def get_models(self):
+        url = f"{self.api_url}/models"
+        
+        response = requests.get(url, headers=self.headers)
+        
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Error: {response.status_code}, {response.text}")
+    
     def send_message(self, message):
+        url = f"{self.api_url}/chat/completions"
+
+        print(self.conversation_history)
+        
+        user_message = {"role": "user", "content": message}
+        self.conversation_history.append(user_message)
+        messages = self.conversation_history.copy()
+        
+        payload = {
+            "model": self.model,
+            "messages": messages
+        }
+
+        response = requests.post(url, headers=self.headers, json=payload)
+        
+        if response.status_code == 200:
+            response_data = response.json()
+            
+            if "choices" in response_data and len(response_data["choices"]) > 0:
+                response_msg = response_data["choices"][0]["message"]
+                self.conversation_history.append(response_msg)
+                
+            return response_data
+        else:
+            raise Exception(f"Error: {response.status_code}, {response.text}")
+    
+    def clear_history(self):
+        self.conversation_history = []
+        
+    def create_chat(self):
         pass
+    
 
 
 def main():
